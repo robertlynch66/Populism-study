c6a484d2a09c4badc6e95f989cc7fad3bd3387ca
Robert Lynch <robertflynch@gmail.com>
2018-09-21 13:44:13 +0300

    Uploading model selection code and rethinking code

--- aggregated binomial.R
+++ aggregated binomial.R
@@ -17,7 +17,7 @@ votes<-map(
 #like depts in UC Berkely data
 #I think I want counties to be entered as varying intercept--
 #data structure of d
-  county suicides_per_capita trump_votes hillary_votes total_votes 
+#  county suicides_per_capita trump_votes hillary_votes total_votes 
 1    1            1                 512          313          825  
 2    2            2                  89           19          108    
 3    3            2                 353          207          560    
@@ -33,7 +33,7 @@ vote_vi<- map2stan(
     admit~ dbinom(applications, p),
     # indexed intercepts for all 6 depts
     logit(p) <- a[county_id] + bs*suicides_per_capita,
-    # the prior for the intercepts mus be indexed too since we are doing them one at a time
+    # the prior for the intercepts must be indexed too since we are doing them one at a time
     a[dept_id] ~ dnorm (0,10), 
     bm~ dnorm (0,10)
   ), data=d)
@@ -42,4 +42,4 @@ vote_vi<- map2stan(
 # comare funtion and see if this model beats the single intercept model
 compare(votes, votes_vi)
 
-#Also do a postcheck to see if the single interceot retordicts the data
+#Also do a postcheck to see if the single intercept retordicts the data
